2d043af70499315705bc90e57aa7cbd93c881502
raggarw6 <165744228+raggarw6@users.noreply.github.com>
2024-11-05 23:57:22 -0800

    Update threads_scheduler.py

--- threads_scheduler.py
+++ threads_scheduler.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
             print("ERROR: Threads scraper aborted\n", error)
 
     # Schedule the Threads scraper job to run every hour at the start of the hour
-    schedule.every().hour.at(":00").do(run_threads_scraper)
+    schedule.every(30).minutes.do(run_threads_scraper)
 
     # Keep the script running and check for pending tasks every 30 seconds
     while True:
